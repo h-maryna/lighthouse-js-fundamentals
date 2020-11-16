513f0abd11ec75626afa5f2a87981da69eaa1e16
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-16 02:16:34 +0000

    Updated hello.js was added

--- hello.js
+++ hello.js
@@ -3,11 +3,23 @@
 }
 sayHello();*/
 
-const sayHello = function(name){
+/*const sayHello = function(name){
     console.log("Hello, " + name)
 }
 sayHello("Carry!");
 sayHello("Caliban");
 sayHello("Miranda");
-sayHello("Ferdinand");
+sayHello("Ferdinand");*/
+
+/*const sayHelloToConsole = function (name){
+    console.log("Hello, " + name);
+}
+
+sayHelloToConsole("John");*/
+
+const returnSayHello = function (name){
+    return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
 
