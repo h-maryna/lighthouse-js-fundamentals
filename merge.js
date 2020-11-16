43b3bbc53a53652b295e8efb11bc1371c5e566ec
Maryna Haidashevska <marynahaidashevska@gmail.com>
2020-11-16 02:24:30 +0000

    Merge function was added

---
+++ merge.js
@@ -0,0 +1,8 @@
+function merge (arr1, arr2){
+  var concat1 = [];
+  concat1 = arr1.concat(arr2);
+  return concat1;
+  }
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]));
+console.log(merge([ 4 ], [ 2, 5, 8 ]));
+console.log(merge([ 1, 2, 6 ], []));
